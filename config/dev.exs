69656573dec3512a4421fd78947a0bb46cc44dc0
Nathan Demick <n@demick.org>
2022-09-05 11:18:02 -0400

    Allow access to app within Docker container

--- config/dev.exs
+++ config/dev.exs
@@ -14,9 +14,7 @@ config :time_tracker, TimeTracker.Repo,
 # watchers to your application. For example, we use it
 # with esbuild to bundle .js and .css sources.
 config :time_tracker, TimeTrackerWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
