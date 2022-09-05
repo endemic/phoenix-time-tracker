3eee0ea540dc5c53fae7378dc14755d363d0e84a
Nathan Demick <n@demick.org>
2022-09-05 11:38:16 -0400

    Add some helper scripts

--- config/dev.exs
+++ config/dev.exs
@@ -14,7 +14,7 @@ config :time_tracker, TimeTracker.Repo,
 # watchers to your application. For example, we use it
 # with esbuild to bundle .js and .css sources.
 config :time_tracker, TimeTrackerWeb.Endpoint,
-  http: [ip: {0, 0, 0, 0}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: System.get_env("PORT") || 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
